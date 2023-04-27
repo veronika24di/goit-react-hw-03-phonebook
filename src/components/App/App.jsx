4d67d66901a01ebff4374cb1ff8f4ff7a458be86
veronika24di <veronikanof909@gmail.com>
2023-04-27 14:13:33 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -33,6 +33,20 @@ export default class App extends Component {
     }));
   };
 
+   componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+
   render() {
     const { contacts, filter } = this.state;
     const optimizedFilter = filter.toLowerCase();
